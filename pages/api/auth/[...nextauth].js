c75a7a21403f708ba2e9eed8d39953a9caf72a54
iamdonprince <princekumax@gmail.com>
2021-12-14 20:52:27 +0530

    feat: adds nextauth provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,16 @@
 import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
-
+import LOGIN_URL from "../../../lib/spotify";
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     SpotifyProvider({
       clientId: process.env.SPOTIFY_ID,
       clientSecret: process.env.SPOTIFY_SECRET,
+      authorization: LOGIN_URL,
     }),
     // ...add more providers here
   ],
+  secret: process.env.JWT_SECRET,
+  pages: { signIn: "/login" },
 });
