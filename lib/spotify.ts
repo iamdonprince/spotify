690a74bb23d25af31f0900c07f821f2badf69957
iamdonprince <princekumax@gmail.com>
2021-12-14 20:52:08 +0530

    feat: adds spotify credential

--- lib/spotify.ts
+++ lib/spotify.ts
@@ -1,1 +1,32 @@
-export const LOG_URL = "";
+const SpotifyWebApi = require("spotify-web-api-node");
+
+const scopes = [
+  "user-read-private",
+  "user-read-email",
+  "playlist-read-private",
+  "playlist-read-collaborative",
+  "streaming",
+  "user-library-read",
+  //   "user-library-modify",
+  "user-top-read",
+  "user-read-playback-state",
+  "user-modify-playback-state",
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-follow-read",
+].join(",");
+
+const params = {
+  scope: scopes,
+};
+
+const queryParams = new URLSearchParams(params);
+export const LOGIN_URL =
+  "https://accounts.spotify.com/authorize" + queryParams.toString();
+
+let spotifyApi = new SpotifyWebApi({
+  clientId: process.env.SPOTIFY_ID,
+  clientSecret: process.env.SPOTIFY_SECRET,
+});
+
+export default spotifyApi;
